8f008860802af5e39b437aafb6fbbd7c08be5579
DmitriyAl <dvalbot@mail.ru>
2016-01-10 17:01:36 +0300

    just test with sout in Main class

--- src/Main.java
+++ src/Main.java
@@ -8,6 +8,7 @@ public class Main {
     public static void main(String[] args) {
         GameWithComputer game = GameWithComputer.getInstance();
         StartMenuViewController start = StartMenuViewController.getInstance(game);
+        System.out.println("smth");
 
     }
 }
